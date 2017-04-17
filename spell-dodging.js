1bdc1a5d66bae95f9ef189b4024c8705284745de
Thomas Cross <thomas.corey.cross@gmail.com>
2017-04-17 22:45:42 +0000

    Day Three

--- spell-dodging.js
+++ spell-dodging.js
@@ -1,13 +1,27 @@
 
 $("body").keydown(function(event) {
     if (event.which === 37) {
-        $("#pic").css("margin-left", $("#pic").offset().left - 42);
-        console.log($("#pic").offset().left);
+        if ($("#pic").offset().left >= 12) {
+            $("#pic").css("margin-left", $("#pic").offset().left - 42);
+            console.log($("#pic").offset().left);
+        }
     } else if (event.which === 39) {
-        $("#pic").css("margin-left", $("#pic").offset().left + 20);
-        console.log($("#pic").offset().left);
+        if ($("#pic").offset().left <= 1250) {
+            $("#pic").css("margin-left", $("#pic").offset().left + 20);
+            console.log($("#pic").offset().left);
+        }
     } else {
         return;
     }
     
 });
+
+setInterval(function() {
+    if($('#box2').offset().top < 550){
+        console.log($('#box2').offset().top);
+        $('#box2').css('margin-top', $('#box2').offset().top + 25);
+    }
+    else{
+        $('#box2').hide();
+    }
+}, 500);
